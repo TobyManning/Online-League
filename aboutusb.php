9807e627f735ac567bdc2d24a31567c9e6af532a
John M Collins <jmc@xisl.com>
2009-09-01 18:35:39 +0100

    Add blurb for About Us

--- aboutusb.php
+++ aboutusb.php
@@ -6,6 +6,32 @@ include 'php/head.php';
 ?>
 <body>
 <h1>About the League</h1>
-<p>Sorry I haven't written this bit yet, please watch this space.</p>
+<p>
+The British Go Association League Tournament is a team-based Go league designed to be
+played mainly over the Internet.
+The purpose of the league is to encourage interaction between players in
+different areas of the country and to encourage online play amongst members of the BGA.
+</p>
+<p>
+The league is played in six-monthly seasons, starting in October and April respectively.
+Teams of up to six players may enter the tournament, and are assigned to leagues of six
+teams. Each season, teams play a round robin tournament within their league.
+Games are played online on the <a href="http://www.gokgs.com">KGS Go Server</a>.
+</p>
+<p>
+The winners in each league are promoted at the end of the season,
+whilst losers face demotion to the league below.
+The champions of the top "A" league will win the League Tournament Cup.
+</p>
+<p>If you would like to form a team, please contact (admin email address).
+Players wishing to join or captain a team must be members of the British Go Association,
+which can be joined <a href="http://www.britgo.org/join">here</a>,
+although one non-member will be allowed per team at a small cost.
+As the league is intended to mainly link to clubs,
+please try to form teams from locally-based players.
+</p>
+<p>You can see the current league status
+<a href="league.php" target="_top">here</a>.
+For any further questions, please contact (team admin address).</p>
 </body>
 </html>
