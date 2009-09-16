389993ca06d29379605f031026b4f11382d88a41
John M Collins <jmc@xisl.com>
2009-09-16 15:43:59 +0100

    And again.

--- updindplayer2.php
+++ updindplayer2.php
@@ -63,19 +63,12 @@ case 'A':
 	$player->create();
 	// If no password specified, invent one
 	if ($strlen($passw) == 0)  {
-//		$poss = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//		$lp = strlen($poss) - 1;
-//		$passw = $passw . $poss[rand(0,$lp)];
-//		$passw = $passw . $poss[rand(0,$lp)];
-//		$passw = $passw . $poss[rand(0,$lp)];
-//		$passw = $passw . $poss[rand(0,$lp)];
-//		$passw = $passw . $poss[rand(0,$lp)];
-//		$passw = $passw . $poss[rand(0,$lp)];
-//		$passw = $passw . $poss[rand(0,$lp)];
-//		$passw = $passw . $poss[rand(0,$lp)];
-		$passw = "Carrot";
+		$poss = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		$lp = strlen($poss) - 1;
+		for ($i = 0; $i < 8; $i++)
+			$passw = $passw . $poss[rand(0,$lp)];
 	}
-	$player->set_passwd($passw);
+	//$player->set_passwd($passw);
 	$Title = "Player {$player->display_name()} created OK";
 	if (strlen($email) != 0)  {
 		$fh = popen("mail -s 'BGA League account created' $email", "w");
