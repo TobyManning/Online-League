936c4d080aafb61bb8d80324f4cb46cfdd645a61
John M Collins <jmc@xisl.com>
2009-11-01 09:29:36 +0000

    Add GPL copyright messages

--- php/nameclash.php
+++ php/nameclash.php
@@ -7,6 +7,21 @@
 <h1>Name Clash</h1>
 <p>
 <?php
+//   Copyright 2009 John Collins
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 print <<<EOT
 Your value for $column of $value clashes with an existing entry.
 EOT;
--- php/opendatabase.php
+++ php/opendatabase.php
@@ -1,9 +1,25 @@
 <?php
 
+//   Copyright 2009 John Collins
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 //  REMEMBER TO HACK THIS - Live requires username/password etc
 //  ***********************************************************
 
-//  ALSO MAKE SURE that magic_quotes_gpc is turned off in you php init!!!!
+//  ALSO MAKE SURE that magic_quotes_gpc is turned off in your php init!!!!
 //  **********************************************************************
 
 if  (!mysql_connect("db48c.pair.com", "maproom_4", "QeWwhsLj")  ||  !mysql_select_db("maproom_bgaleague")) {
