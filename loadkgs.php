634b47f707d79ca1ee4136b53203160451ee9898
John M Collins <jmc@xisl.com>
2009-11-13 16:53:08 +0000

    Add prog name to message

--- loadkgs.php
+++ loadkgs.php
@@ -53,7 +53,7 @@ if ($fh)  {
 	if ($code != 0 || strlen($sgfdata) == 0)  {
 		switch ($code) {
 		default:
-			$msg = "I cannot tell why code was $code";
+			$msg = "I cannot tell why code was $code (prog $prog)";
 			break;
 		case 10:
 			$msg = "Could not find games on {$date_played->display()}";
