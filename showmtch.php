ba193cc3822807b72860e5bd14a7c4dd0a09b627
John M Collins <jmc@xisl.com>
2010-04-08 18:23:23 +0100

    Fix name

--- showmtch.php
+++ showmtch.php
@@ -38,7 +38,7 @@ try  {
 }
 catch (MatchException $e) {
 	if ($e->Nfound) {
-		$loc = "histmatch.php?hmi={$e->Mid}";
+		$loc = "histshowmatch.php?hmi={$e->Mid}";
 		include 'php/jumpto.php';
 		exit(0);
 	}
