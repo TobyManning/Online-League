17e139208b91f5ee45f3f9393ce7bbfb4039c434
John M Collins <jmc@xisl.com>
2010-05-23 20:50:24 +0100

    Revise reminder messages to mail players individually.

--- reminder.php
+++ reminder.php
@@ -8,134 +8,128 @@ include 'php/teammemb.php';
 include 'php/match.php';
 include 'php/matchdate.php';
 include 'php/game.php';
+include 'php/params.php';
+
+// Remember about player we can't email to bleat at team captain about instead
+
+class aboutplayer {
+	public $player;		// Player we are talking about
+	public $opp;			// His/her opponent
+	public $match;			// Match info
+	public $game;			// Game info
+	public $reason;		// Reason not emailed
+	
+	public function __construct($p, $o, $m, $g, $r) {
+		$this->player = $p;
+		$this->opp = $o;
+		$this->match = $m;
+		$this->game = $g;
+		$this->reason = $r;
+	}
+}
+
+// Stuff to remember to tell team captains about
 
 class tcrems {
 
-	public $Capt;
-	public $Unalloc;
-	public $Notplayed;
-	public $Partplayed;
+	public $Capt;			// The poor sucker
+	public $Unalloc;		// Unallocated matches
+	public $Noemail;		// No emails to players
 	
 	public function __construct($c) {
 		$this->Capt = $c;
 		$this->Unalloc = array();
-		$this->Notplayed = array();
-		$this->Partplayed = array();
+		$this->Noemail = array();
 	}
 	
 	public function addmatch($m) {
-		if ($m->Result == 'N')  {
-			if ($m->is_allocated())
-				array_push($this->Notplayed, $m);
-			else
-				array_push($this->Unalloc, $m);
-		}
-		else
-			array_push($this->Partplayed, $m);
+		array_push($this->Unalloc, $m);
 	}
-	
+
+	public function addgame($p) {
+		array_push($this->Noemail, $p);
+	}
+		
 	public function othercapt($m) {
 		if ($this->Capt->is_same($m->Hteam->Captain))
 			return $m->Ateam->Captain;
 		return $m->Hteam->Captain;
 	}
-	
-	public function mails() {
-		$dest = $this->Capt->Email;
-		$fh = popen("mail -s 'Go League match reminder' $dest", "w");
-		$mess = <<<EOT
-
-Dear {$this->Capt->display_name(false)},
-
-This is an auto-reminder from the BGA online league software.
-Please do not reply to this!
-
-EOT;
-		fwrite($fh, $mess);
-
-		if (count($this->Unalloc) != 0)  {
-			$mess = <<<EOT
-
-Please can you complete the allocation of teams to the following matches.
-The other team captains are given in ()s.
-
-
-EOT;
-			fwrite($fh, $mess);
-			foreach ($this->Unalloc as $m) {
-				$oth = $this->othercapt($m);
-				$mess = <<<EOT
-{$m->Date->display_month()} {$m->Hteam->display_name()} -v- {$m->Ateam->display_name()} ({$oth->display_name(false)} {$oth->Email} {$oth->Phone})
-
-EOT;
-				fwrite($fh, $mess);
-			}
-		}
-
-		if (count($this->Notplayed) != 0)  {
-			$mess = <<<EOT
-
-Please can you start playing the following matches.
-The other team captains are given in ()s.
-
-
-EOT;
-			fwrite($fh, $mess);
-			foreach ($this->Notplayed as $m) {
-				$oth = $this->othercapt($m);
-				$mess = <<<EOT
-{$m->Date->display_month()} {$m->Hteam->display_name()} -v- {$m->Ateam->display_name()} ({$oth->display_name(false)} {$oth->Email} {$oth->Phone})
-
-
-EOT;
-				fwrite($fh, $mess);
-			}
-		}
-		
-		if (count($this->Partplayed) != 0)  {
-			$mess = <<<EOT
-
-Please can you arrange to complete the following matches - outstanding games listed.
-The other team captains are given in ()s.
-
-
-EOT;
-			fwrite($fh, $mess);
-			foreach ($this->Partplayed as $m) {
-				$oth = $this->othercapt($m);
-				$mess = <<<EOT
-{$m->Date->display_month()} {$m->Hteam->display_name()} -v- {$m->Ateam->display_name()} ({$oth->display_name(false)} {$oth->Email} {$oth->Phone})
-
-EOT;
-				fwrite($fh, $mess);
-				foreach ($m->Games as $g) {
-					if ($g->Result != 'N')
-						continue;
-					$mess = <<<EOT
-  {$g->Wplayer->display_name(false)} ({$g->Wplayer->display_online()}) of {$g->Wteam->display_name()} -v- {$g->Bplayer->display_name(false)} ({$g->Bplayer->display_online()}) of {$g->Bteam->display_name()}
-
-EOT;
-					fwrite($fh, $mess);
-				}
-			}
-		}
-		
-		$mess = <<<EOT
-
-If you have allocated teams/played matches listed above please enter them
-on the website.
-
-Thanks - league auto-reminder service.
-
-EOT;
-		fwrite($fh, $mess);
-		pclose($fh);
-	}
 }
+
+// Allocate a new tcrem structure if needed
+
+function gettcrem($tc) {
+	global $Captains;
+	$cn = $tc->display_name(false);
+	if  (!array_key_exists($cn, $Captains))
+		$Captains[$cn] = new tcrems($tc);
+	return $Captains[$cn];
+}
+
+// Generate a mail message to the player about impending match
+// or thinking of a reason why not
+
+function mailplayer($play, $opp, $col, $mt, $ot, $m, $g)
+{
+	global $pars;
+	if  (strlen($play->Email) == 0)
+		return new aboutplayer($play, $opp, $m, $g, "has given no email address");
+	if  (!$play->OKemail)
+		return new aboutplayer($play, $opp, $m, $g, "has not agreed to auto-email");
+	$dest = $play->Email;
+	$fh = popen("mail -s 'Online league match reminder' $dest", "w");
+	$mess = <<<EOT
+Dear {$play->display_name(false)}
+
+Please can we remind you that your are due to play in the online league match
+playing for {$mt->display_name()} against {$ot->display_name()}.
+
+Your opponent is {$opp->display_name(false)} {$opp->display_rank()}.
+
+You are playing as $col.
+
+EOT;
+	fwrite($fh, $mess);
+	
+	$hcapable = $m->Division >= $pars->Hdiv;
+	$hred = $pars->Hreduct;
+	$hstones = 0;
+	if ($hcapable)  {
+		$hstones = $g->Wplayer->Rank->Rankvalue - $g->Bplayer->Rank->Rankvalue - $hred;
+		if ($hstones > 9)
+			$hstones = 9;
+	}
+	if ($hstones > 0)  {
+		if ($hstones == 1)
+			fwrite($fh, "\nPlease note this game is played with No Komi\n");
+		else
+			fwrite($fh, "\nPlease note this game is played with a $hstones stone handicap\n");
+	}
+	$capt = $mt->Captain;
+	if (!$capt->is_same($play))  {
+		$mess = <<<EOT
+
+If you have any questions, please contact your team captain, {$capt->display_name(false)},
+whose email address is {$capt->Email}.
+
+EOT;
+		fwrite($fh, $mess);
+	}
+	pclose($fh);
+	return false;
+}
+
+// Get parameters to include handicap info
+
+$pars = new Params();
+$pars->fetchvalues();
+
+// Array of team captains to bleat to
 
 $Captains = array();
 
-$ret = mysql_query("select ind from lgmatch where result!='H' and result!='A' and matchdate < date_add(current_date(),interval 45 day) order by divnum,matchdate,result");
+$ret = mysql_query("select ind from lgmatch where result!='H' and result!='A' and matchdate < date_add(current_date(),interval 30 day) order by divnum,matchdate,result");
 if ($ret && mysql_num_rows($ret) > 0)  {
 	while ($row = mysql_fetch_array($ret))  {
 		$ind = $row[0];
@@ -148,21 +142,115 @@ if ($ret && mysql_num_rows($ret) > 0)  {
 		catch (MatchException $e) {
 			continue;
 		}
-		$hc = $mtch->Hteam->Captain;
-		$ac = $mtch->Ateam->Captain;
-		$hcn = $hc->display_name(false);
-		$acn = $ac->display_name(false);
+		$ht = $mtch->Hteam;
+		$at = $mtch->Ateam;
+		$hc = $ht->Captain;
+		$ac = $at->Captain;
 		
-		if  (!array_key_exists($hcn, $Captains))
-			$Captains[$hcn] = new tcrems($hc);
-		if  (!array_key_exists($acn, $Captains))
-			$Captains[$acn] = new tcrems($ac);
-		
-		$Captains[$hcn]->addmatch($mtch);
-		$Captains[$acn]->addmatch($mtch);
+		if ($mtch->is_allocated())  {
+			
+			// Match is allocated, find each unplayed game
+
+			foreach ($mtch->Games as $g)  {
+				if ($g->Result != 'N')
+					continue;
+				if ($g->Wteam->is_same($ht)) {
+					$hp = $g->Wplayer;
+					$ap = $g->Bplayer;
+					$hcol = "White";
+					$acol = "Black";
+				}
+				else  {
+					$hp = $g->Bplayer;
+					$ap = $g->Wplayer;
+					$hcol = "Black";
+					$acol = "White";
+				}
+				$r = mailplayer($hp, $ap, $hcol, $ht, $at, $mtch, $g);
+				if  ($r)
+					gettcrem($hc)->addgame($r);
+				$r = mailplayer($ap, $hp, $acol, $at, $ht, $mtch, $g);
+				if  ($r)
+					gettcrem($ac)->addgame($r);
+			}
+		}
+		else {
+			gettcrem($hc)->addmatch($mtch);
+			gettcrem($ac)->addmatch($mtch);
+		}
+	}
+}
+
+foreach ($Captains as $capt)  {
+	$dest = $capt->Capt->Email;
+	$fh = popen("mail -s 'Go League match reminder' $dest", "w");
+	$mess = <<<EOT
+
+Dear {$capt->Capt->display_name(false)},
+
+This is an auto-reminder from the BGA online league software.
+Please do not reply to this!
+
+EOT;
+	fwrite($fh, $mess);
+
+	if (count($capt->Unalloc) != 0)  {
+		$mess = <<<EOT
+
+Please can you complete the allocation of teams to the following matches and/or chase
+the opposing team captain to do so.
+
+
+EOT;
+		fwrite($fh, $mess);
+		foreach ($capt->Unalloc as $m) {
+			$oth = $capt->othercapt($m);
+			$mess = <<<EOT
+Date: {$m->Date->display_month()}
+Between: {$m->Hteam->display_name()} -v- {$m->Ateam->display_name()}
+Other team captain: ({$oth->display_name(false)} {$oth->Email} {$oth->Phone})
+
+
+EOT;
+			fwrite($fh, $mess);
+		}
 	}
 
-	foreach (array_keys($Captains) as $cn)
-		$Captains[$cn]->mails();
+	if (count($capt->Noemail) != 0)  {
+		$mess = <<<EOT
+
+Please can you encourage your team members to play the following games,
+I couldn't email them directly for the reasons given.
+
+
+EOT;
+		fwrite($fh, $mess);
+		foreach ($capt->Noemail as $noem) {
+			$m = $noem->match;
+			$pl = $noem->player;
+			$opp = $noem->opp;
+				
+			$mess = <<<EOT
+Date: {$m->Date->display_month()}
+Match: {$m->Hteam->display_name()} -v- {$m->Ateam->display_name()}
+Game: {$pl->display_name(false)} -v- {$opp->display_name(false)}
+Reason: Player {$noem->reason}
+
+
+EOT;
+			fwrite($fh, $mess);
+		}
+	}
+		
+	$mess = <<<EOT
+
+If you have allocated teams/played matches listed above please enter them
+on the website.
+
+Thanks - league auto-reminder service.
+
+EOT;
+	fwrite($fh, $mess);
+	pclose($fh);
 }
 ?>
