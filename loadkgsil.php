297ba2fcee1ce5d35db187aab77561ac23131e62
John M Collins <jmc@xisl.com>
2010-07-14 16:05:51 +0100

    Add missing include

--- loadkgsil.php
+++ loadkgsil.php
@@ -41,6 +41,7 @@ include 'php/club.php';
 include 'php/rank.php';
 include 'php/player.php';
 include 'php/matchdate.php';
+include 'php/team.php';
 include 'php/game.php';
 include 'php/kgsfetchsgf.php';
 include 'php/news.php';
