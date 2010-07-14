2cf437dc04840339b1ddb7af7a1c8b3d83e26c2f
John M Collins <jmc@xisl.com>
2010-07-14 16:17:05 +0100

    Missed create_game call.

--- loadkgsil.php
+++ loadkgsil.php
@@ -157,6 +157,7 @@ try {
 catch  (GameException $e)  {
 	$msg = htmlspecialchars($e->getMessage());
 }
+$g->create_game();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
