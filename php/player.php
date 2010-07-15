31b8bba9bc18b188eed499f83b9dfe20b84b5509
John M Collins <jmc@xisl.com>
2010-07-15 09:46:43 +0100

    Fix cross-table to give individual league games only!

--- php/player.php
+++ php/player.php
@@ -562,7 +562,7 @@ class Player  {
 	}
 
 	private function getcount($seasind, $q) {
-		$ret = mysql_query("select count(*) from game where seasind=$seasind and $q");
+		$ret = mysql_query("select count(*) from game where league='I' and seasind=$seasind and $q");
 		if (!$ret || mysql_num_rows($ret) == 0)
 			return  0;
 		$row = mysql_fetch_array($ret);
