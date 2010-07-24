548b2c7a3b5322e008340fbdd2d67f6842596be6
John M Collins <jmc@xisl.com>
2010-07-24 16:01:24 +0100

    Add item to log news entries about league s/w updates.

---
+++ lognews.pl
@@ -0,0 +1,28 @@
+#! /usr/bin/perl
+
+use DBD::mysql;
+
+$Database = DBI->connect("DBI:mysql:database=maproom_bgaleague;host=db48c.pair.com", "maproom_4", "QeWwhsLj");
+
+die "Cannot open git" unless open(LG, "git log --no-color -n 1|");
+
+while (<LG>)  {
+    chop;
+    $lastline = $_;
+    if (/^Author:\s*.*<(.*)@.*>/) {
+	$auth = $1;
+	next;
+    }
+}
+
+$lastline =~ s/^\s*(.*)\s*$/$1/;
+exit 0 unless length($lastline) != 0;
+$lastline = "Software update: $lastline";
+$auth = "ADMINS" unless $auth;
+$qlog = $Database->quote($lastline);
+$quser = $Database->quote($auth);
+$sfh = $Database->prepare("insert into news (ndate,user,item) values (current_date,$quser,$qlog)");
+$sfh->execute();
+exit 0;
+
+
