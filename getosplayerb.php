bfe81b562fcae6cf318349c82057c83ab153fee9
John M Collins <jmc@xisl.com>
2010-08-13 22:45:22 +0100

    Add missing include.

--- getosplayerb.php
+++ getosplayerb.php
@@ -19,6 +19,7 @@ include 'php/checklogged.php';
 include 'php/opendatabase.php';
 include 'php/rank.php';
 include 'php/player.php';
+include 'php/club.php';
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
