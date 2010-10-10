8cedba22818650a74782df10bfc305bed8a87296
John M Collins <jmc@xisl.com>
2010-10-10 15:40:17 +0100

    Fix database change in news log.

--- lognews.pl
+++ lognews.pl
@@ -2,7 +2,7 @@
 
 use DBD::mysql;
 
-$Database = DBI->connect("DBI:mysql:database=maproom_bgaleague;host=db48c.pair.com", "maproom_4", "QeWwhsLj");
+$Database = DBI->connect("DBI:mysql:bgaleague", "www-data", "BGA league access") or die "Cannot open DB";
 
 die "Cannot open git" unless open(LG, "git log --no-color -n 1|");
 
