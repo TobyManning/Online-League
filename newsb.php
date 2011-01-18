4015d7a40bde2111b1a169e84cb89a324fd11f1a
John M Collins <jmc@xisl.com>
2011-01-18 19:33:13 +0000

    Include missing includes.

--- newsb.php
+++ newsb.php
@@ -16,6 +16,9 @@
 
 include 'php/session.php';
 include 'php/opendatabase.php';
+include 'php/club.php';
+include 'php/rank/php';
+include 'php/player.php';
 include 'php/matchdate.php';
 include 'php/news.php';
 ?>
