ea49a7b26ace2bd33e2ad8a068c846339585d3eb
John M Collins <jmc@xisl.com>
2011-02-12 09:09:05 +0000

    Limit add SGF function to current games.

--- php/game.php
+++ php/game.php
@@ -385,7 +385,7 @@ class Game {
 
 function list_nosgf_games() {
 	$result = array();
-	$ret = mysql_query("select ind from game where result!='N' and length(sgf)=0 order by matchdate,wrank desc,brank desc");
+	$ret = mysql_query("select ind from game where result!='N' and length(sgf)=0 and current=1 order by matchdate,wrank desc,brank desc");
 	if ($ret) {
 		while ($row = mysql_fetch_array($ret)) {
 			$r = new game($row[0]);
