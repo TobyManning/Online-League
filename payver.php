23e6e3c3fc2b303837119d7d3b71fd49efc89a70
John M Collins <jmc@xisl.com>
2012-01-12 20:59:10 +0000

    Paypal Get Details stage.

---
+++ payver.php
@@ -0,0 +1,221 @@
+<?php
+//   Copyright 2012 John Collins
+
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+include 'php/checksecure.php';
+include 'php/session.php';
+include 'php/checklogged.php';
+include 'php/opendatabase.php';
+include 'php/club.php';
+include 'php/rank.php';
+include 'php/player.php';
+include 'php/team.php';
+include 'php/teammemb.php';
+
+// This is the module called when all goes OK with the initial transaction, we need to get details of the
+// transaction from Paypal and set up to call the final confirmation.
+
+function apiapp(&$arr, $k, $v) {
+	array_push($arr, "$k=$v");
+}
+
+try {
+	$player = new Player();
+	$player->fromid($userid);
+}
+catch (PlayerException $e) {
+	$mess = $e->getMessage();
+	include 'php/wrongentry.php';
+	exit(0);
+}
+
+$ind = $_GET["ind"];
+$tok = $_GET["TOKEN"];
+if (strlen($ind) == 0 || strlen($tok) == 0)  {
+	$mess = "No indicator given ind=$ind tok=$tok???";
+	include 'php/wrongentry.php';
+	exit(0);
+}
+
+$ret = mysql_query("select league,descr1,descr2,token from pendpay where ind=$ind");
+if (!$ret)  {
+	$mess = mysql_error();
+	include 'php/dataerror.php';
+	exit(0);
+}
+if (mysql_num_rows($ret) == 0)  {
+	$mess = "Cannot find pending payment, ind=$ind";
+	include 'php/wrongentry.php';
+	exit(0);
+}
+$row = mysql_fetch_assoc($ret);
+
+// Verify that the token matches up (change this later not to display them)
+
+$rtok = $row["token"];
+if ($tok != $rtok) {
+	$mess = "Mismatch tokens r=$tok, d=$rtok";
+	include 'php/wrongentry.php';
+	exit(0);
+}
+
+switch  ($row["league"])  {
+default:
+	$mess = "Do not know how to do {$row['league']} payments yet";
+	include 'php/wrongentry.php';
+	exit(0);
+case  'T':
+	$type = 'T';
+	$teamname = $row["descr1"];
+	break;
+case  'I':
+	$type = 'I';
+	$first = $row["descr1"];
+	$last = $row["descr2"];
+	break;
+}
+
+try {
+	if ($type == 'T')  {
+		$team = new Team($teamname);
+		$team->fetchdets();
+		
+		// Error if this team has paid
+		
+		if ($team->Paid)  {
+			$mess = "Team $teamname have already paid??";
+			include 'php/wrongentry.php';
+			exit(0);
+		}
+	}
+	else  {
+		$pplayer = new Player($first, $last);
+		$pplayer->fetchdets();
+		
+		// Error if this player has paid
+		
+		if ($pplayer->ILpaid)  {
+			$mess = "$first $last is already paid??";
+			include 'php/wrongentry.php';
+			exit(0);
+		}
+	}
+}
+catch (PlayerException $e) {
+	$mess = $e->getMessage();
+	include 'php/wrongentry.php';
+	exit(0);
+}
+catch (TeamException $e) {
+	$mess = $e->getMessage();
+	include 'php/wrongentry.php';
+	exit(0);
+}
+
+// OK now we are ready to do the PayPal stuff stage 3.
+// HERE ARE THE CREDENTIALS
+
+$API_UserName = urlencode('jmc_1326312017_biz_api1.xisl.com');
+$API_Password = urlencode('1326312045');
+$API_Signature = urlencode('AFcWxV21C7fd0v3bYYYRCpSSRl31AIiKoYf.QsZ4OwXr2K59wxqse3Jq');
+$API_Endpoint = "https://api-3t.sandbox.paypal.com/nvp";
+
+// Step 3 is to get the details
+
+$Req_array = array();
+apiapp($Req_array, "METHOD", "GetExpressCheckoutDetails");
+apiapp($Req_array, "VERSION", urlencode('51.0'));
+apiapp($Req_array, "USER", $API_UserName);
+apiapp($Req_array, "PWD", $API_Password);
+apiapp($Req_array, "SIGNATURE", $API_Signature);
+apiapp($Req_array, "TOKEN", urlencode($tok));
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $API_Endpoint);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, join('&', $Req_array));
+$chresp = curl_exec($ch);
+if  (!$chresp)  {
+	$mess = "Curl failed: " . curl_error($ch) . " (" . curl_errno($ch) . ")";
+	include 'php/probpay.php';
+	exit(0);
+}
+
+// Make an array of the response
+
+$responses = explode('&', $chresp);
+$parsedresp = array();
+foreach ($responses as $r) {
+	$ra = explode('=', $r);
+	if (count($ra) > 1)
+		$parsedresp[$ra[0]] = urldecode($ra[1]);
+}
+
+// Check success
+
+if ($parsedresp["ACK"] != 'Success')  {
+	$mess = "API error in Set Express Checkout";
+	mysql_query("delete from pendpay where ind=$ind");
+	include 'php/probpay.php';
+	exit(0);
+}
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<?php
+$Title = "Please confirm payment";
+include 'php/head.php';
+?>
+<body>
+<?php include 'php/nav.php'; ?>
+<h1>Payment OK</h1>
+<?php
+if ($type == 'T') {
+	print <<<EOT
+<p>Recording payment on behalf of {$team->display_name()}.</p>
+
+EOT;
+}
+else {
+	print <<<EOT
+<p>Recording payment on behalf of {$pplayer->display_name()}.</p>
+
+EOT;
+}
+print <<<EOT
+<p>Parsed responses from PayPal are:</p>
+<table>
+<tr><th>Param</th><th>Value</th></tr>
+
+EOT;
+
+foreach ($parsedresp as $k => $v) {
+	$qk = htmlspecialchars($k);
+	$qv = htmlspecialchars($v);
+	print <<<EOT
+<tr><td>$qk</td><td>$qv</td></tr>
+
+EOT;
+}
+?>
+</table>
+</div>
+</div>
+</body>
+</html>
