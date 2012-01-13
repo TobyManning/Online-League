7e0c375497aec38a06c7a197a726d7bcdd391ec8
John M Collins <jmc@xisl.com>
2012-01-13 14:35:36 +0000

    Revise logos

--- payments.php
+++ payments.php
@@ -225,7 +225,7 @@ else {
 <tr><td align="center" valign="middle">
 <a href="#" onclick="javascript:window.open('https://www.paypal.com/cgi-bin/webscr?cmd=xpt/Marketing/popup/OLCWhatIsPayPal-outside','olcwhatispaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=350');">
 <img src="https://www.paypal.com/en_US/i/logo/PayPal_mark_180x113.gif" border="0" alt="Acceptance Mark">
-<img src="images/cc_accept.gif" width="183" height="38" alt="CC images" longdesc="Credit card images" /></a></td></tr>
+<img src="images/cc_accept.gif" width="316" height="65" alt="CC images" longdesc="Credit card images" /></a></td></tr>
 </table><!-- PayPal Logo -->
 <form name="payform" action="pppayment.php" method="post" enctype="application/x-www-form-urlencoded">
 <div align="center">
