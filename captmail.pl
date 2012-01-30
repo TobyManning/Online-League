ca97795b85c2b71056bea3755a6b1097f9bf0b3a
John M Collins <jmc@xisl.com>
2012-01-30 14:37:33 +0000

    Add some facilities to captmail.

--- captmail.pl
+++ captmail.pl
@@ -1,8 +1,44 @@
 #! /usr/bin/perl
 
+$playing = 1;
+$paid = 0;
+
+while ($ARGV[0] =~ /^-([APBpu]+)/) {
+	my $lets = $1;
+	while ($lets =~ /(.)/g)  {
+		my $let = $1;
+		if ($let eq 'A')  {
+			$playing = 0;
+		}
+		elsif ($let eq 'P') {
+			$playing = 1;
+		}
+		elsif ($let eq 'B') {
+			$paid = 0;
+		}
+		elsif ($let eq 'p') {
+			$paid = 1;
+		}
+		elsif ($let eq 'u') {
+			$paid = -1;
+		}
+	}
+}
+
+@Conds = ('team.captfirst=player.first', 'team.captlast=player.last', 'length(player.email)>0');
+push @Conds, 'team.playing!=0' if $playing;
+if  ($paid != 0)  {
+	if ($paid < 0) {
+		push @Conds, 'team.paid=0';
+	}
+	else {
+		push @Conds, 'team.paid!=0';
+	}
+}
+
 use DBD::mysql;
 $Database = DBI->connect("DBI:mysql:bgaleague", "www-data", "BGA league access") or die "Cannot open DB";
-$sfh = $Database->prepare("select player.email from player,team where team.playing!=0 and team.captfirst=player.first and team.captlast=player.last and length(player.email)>0");
+$sfh = $Database->prepare("SELECT player.email FROM player,team WHERE " . join(' AND ', @Conds));
 $sfh->execute;
 
 while (@row = $sfh->fetchrow_array) {
@@ -10,6 +46,7 @@ while (@row = $sfh->fetchrow_array) {
 }
 
 $sfh = $Database->prepare("select email from player where admin!='N' and length(email) > 0");
+$sfh->execute;
 while (@row = $sfh->fetchrow_array) {
 	$Emails{$row[0]} = 1;
 }
