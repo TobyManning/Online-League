8e3400699aa477acfa5117c070943febcc218922
John M Collins <jmc@xisl.com>
2013-03-26 14:03:23 +0000

    Display sent messages.

--- messages.php
+++ messages.php
@@ -25,6 +25,80 @@ include 'php/match.php';
 include 'php/matchdate.php';
 include 'php/game.php';
 
+function listmessages($userid, $sent)
+{
+	if ($sent)  {
+		$searchid = 'touser';
+		$fetchid = 'fromuser';
+		$msgt = "Sent";
+		$fl = "&sent=y";
+		$th = "To";
+	}
+	else  {
+		$searchid = 'fromuser';
+		$fetchid = 'touser';
+		$msgt = "Received";
+		$fl = "";
+		$th = "From";
+	}
+	print "<h1>$msgt messages</h1>";
+	$Sun = mysql_real_escape_string($userid);
+	$ret = mysql_query("select $searchid,created,matchind,gameind,subject,hasread,ind from message where $fetchid='$Sun' order by created,subject");
+	if (!$ret || mysql_num_rows($ret) == 0)  {
+		print "<p>No messages found.</p>\n";
+	}
+	else  {
+		print <<<EOT
+<table class="resultsb">
+<tr>
+	<th>$th</th>
+	<th>Date</th>
+	<th>Time</th>
+	<th>Match</th>
+	<th>Game</th>
+	<th>Subject</th>
+EOT;
+		while ($row = mysql_fetch_assoc($ret))  {
+			$fu = $row[$searchid];
+			$cr = $row["created"];
+			$mid = $row["matchind"];
+			$gid = $row["gameind"];
+			$subj = $row["subject"];
+			$qsubj = htmlspecialchars($subj);
+			$hasr = $row["hasread"];
+			$ind = $row["ind"];
+			$fp = new Player();
+			$fp->fromid($fu);
+			$pre = $post = "";
+			if (!$hasr)  {
+				$pre = "<strong>";
+				$post = "</strong>";			
+			}
+			if  (preg_match("/(\d+)-(\d+)-(\d+)\s+(\d+):(\d+):(\d+)/", $cr, $matches))  {
+				$dat = $matches[3] . '/' . $matches[2] . '/' . $matches[1];
+				$tim = $matches[4] . ':' . $matches[5] . ':' . $matches[6];
+			}
+			else {
+				$dat = $tim = $cr;
+			}
+			$am = $mid == 0? "-": "About match";
+			$ag = $gid == 0? "-": "About game";
+			print <<<EOT
+<tr>
+	<td>$pre{$fp->display_name()}$post</td>
+	<td>$pre$dat$post</td>
+	<td>$pre$tim$post</td>
+	<td>$pre$am$post</td>
+	<td>$pre$ag$post</td>
+	<td><a href="dispmessage.php?mi=$ind$fl">$pre$qsubj$post</a></td>
+</tr>
+
+EOT;
+		}
+		print "</table>\n";
+	}
+}
+
 try {
         $player = new Player();
         $player->fromid($userid);
@@ -82,74 +156,36 @@ include 'php/head.php';
 ?>
 <body>
 <?php include 'php/nav.php';
-print "<h1>Messages for $Qun</h1>\n";
-$ret = mysql_query("select fromuser,created,gameind,subject,hasread,ind from message where touser='$Sun' order by created,subject");
-if (!$ret || mysql_num_rows($ret) == 0)  {
-	print "<p>No received messages for $Qun.</p>\n";
-}
-else  {
-	print <<<EOT
-<p>The following messages have been received for $Qun.</p>
-<table class="resultsb">
-<tr>
-	<th>From</th>
-	<th>Date</th>
-	<th>Time</th>
-	<th>Game</th>
-	<th>Subject</th>
-EOT;
-	while ($row = mysql_fetch_assoc($ret))  {
-		$fu = $row["fromuser"];
-		$cr = $row["created"];
-		$gid = $row["gameind"];
-		$subj = $row["subject"];
-		$qsubj = htmlspecialchars($subj);
-		$hasr = $row["hasread"];
-		$ind = $row["ind"];
-		$fp = new Player();
-		$fp->fromid($fu);
-		$pre = $post = "";
-		if (!$hasr)  {
-			$pre = "<strong>";
-			$post = "</strong>";			
-		}
-		if  (preg_match("/(\d+)-(\d+)-(\d+)\s+(\d+):(\d+):(\d+)/", $cr, $matches))  {
-			$dat = $matches[3] . '/' . $matches[2] . '/' . $matches[1];
-			$tim = $matches[4] . ':' . $matches[5] . ':' . $matches[6];
-		}
-		else {
-			$dat = $tim = $cr;
-		}
-		$ag = $gid == 0? "Other": "About game";
-		print <<<EOT
-<tr>
-	<td>$pre{$fp->display_name()}$post</td>
-	<td>$pre$dat$post</td>
-	<td>$pre$tim$post</td>
-	<td>$pre$ag$post</td>
-	<td><a href="dispmessage.php?mi=$ind">$pre$qsubj$post</a></td>
-</tr>
 
-EOT;
-	}
-	print "</table>\n";
-}
+// List received messages
+
+listmessages($userid, 'false');
+
+// List O/S matches for team captain if this player is such
+
 if  (count($capt_matches) != 0)  {
 	print <<<EOT
 <h1>Outstanding matches</h1>
 <p>You might want to send the opposing team captain a message about
 one of the following outstanding matches.</p>
 <table class="showmatch">
-<tr><th>Team A</th><th>Capt</th><th>Team B</th><th>Capt</th><th>Send</th></tr>
+<tr><th>Team A</th><th>Capt</th><th>Team B</th><th>Capt</th><th>State</th><th>Send</th></tr>
 
 EOT;
 	foreach ($capt_matches as $mtch) {
+		if ($mtch->is_allocated())  {
+			$state = "Unplayed";
+		}
+		else {
+			$state = "Unallocated";
+		}
 		print <<<EOT
 <tr>
 	<td>{$mtch->Hteam->display_name(true)}</td>
 	<td>{$mtch->Hteam->display_captain(true)}</td>
 	<td>{$mtch->Ateam->display_name(true)}</td>
 	<td>{$mtch->Ateam->display_captain(true)}</td>
+	<td>$state</td>
 	<td><a href="composemsg.php?{$mtch->urlof()}">Message</a></td>
 </tr>
 
@@ -157,9 +193,7 @@ EOT;
 	}
 	print "</table>\n";
 }
-?>
-<h1>Outstanding games</h1>
-<?php
+print "<h1>Outstanding games</h1>\n";
 
 // Now for user's games
 
@@ -226,6 +260,7 @@ EOT;
 <h1>General message</h1>
 <p>If you want to send someone a new message about any subject,
 <a href="composegmsg.php">Click Here</a>.</p>
+<?php listmessages($userid, true); ?>
 </div>
 </div>
 </body>
