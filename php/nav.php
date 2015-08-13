e5987537ed158a280fcd4430f5938232f8aa2842
John M Collins <jmc@toad.me.uk>
2015-08-13 21:56:01 +0100

    Revisions for popup menus.

--- php/nav.php
+++ php/nav.php
@@ -1,6 +1,11 @@
 <?php
 //   Copyright 2011 John Collins
 
+// *****************************************************************************
+// PLEASE BE CAREFUL ABOUT EDITING THIS FILE, IT IS SOURCE-CONTROLLED BY GIT!!!!
+// Your changes may be lost or break things if you don't do it correctly!
+// *****************************************************************************
+
 //   This program is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation, either version 3 of the License, or
@@ -29,66 +34,83 @@ EOT;
 <div class="innertube">
 <a href="http://www.britgo.org" title="Go to BGA main site">
 <img src="images/gohead12.gif" width="133" height="47" alt="BGA Logo" border="0" hspace="0" vspace="0"></a>
-<table>
-<tr><td><a href="index.php" title="Go to league home page">League Home</a></td></tr>
-<tr><td><a href="info.php" title="General Information">Information</a></td></tr>
-<tr><td><a href="playing.php" title="How to play in the leagues">Rules</a></td></tr>
-<tr><td><a href="news.php" title="Read about recent results and updates">News</a></td></tr>
-<tr><td><a href="clubs.php" title="View the list of clubs with members in the leagues">Clubs</a></td></tr>
-<tr><td><a href="teams.php" title="View the teams participating in the league">Teams</a></td></tr>
-<tr><td><a href="players.php" title="View players in the league">Players</a></td></tr>
-<tr><td class="subind"><a href="players.php?by=club" title="VIew players sorted by club">By club</a></td></tr>
-<tr><td class="subind"><a href="players.php?by=rank" title="View players sorted by rank">By rank</a></td></tr>
-<tr><td class="subind"><a href="players.php?by=clubrank" title="View players sorted by club then by rank">By club/rank</a></td></tr>
-<tr><td class="subind"><a href="pbt.php" title="View players by team">By team</a></td></tr>
-<tr><td><a href="matches.php" title="View team league matches for current season">Team Matches</a></td></tr>
-<tr><td><a href="results.php" title="View results table for team league matches">Team Results</a></td></tr>
-<tr><th colspan="2">League Table</th></tr>
-<tr><td class="subind"><a href="league.php" title="Display current team league table">Current</a></td></tr>
-<tr><td class="subind"><a href="leaguehist.php" title="Display historical league tables including individual league">Historical</a></td></tr>
-<!-- <tr><td class="subind"><a href="ileague.php" class="il" title="View league table for Individual League">Individual League</a></td></tr> -->
+<ul class="pmen">
+<li><<a href="index.php" title="Go to league home page">League Home</a></li>
+<li><a href="#">Information &#9662;</a>
+<ul class="pdropdown">
+<li><a href="info.php" title="General Information about the league">>Information</a></li>
+<li><a href="playing.php" title="How to play in the league">>League Rules</a></li>
+<li><a href="news.php" title="Read about recent results and update">>League News</a></li>
+<li><a href="clubs.php" title="View the list of clubs with members in the league">>Clubs</a></li>
+<li><a href="teams.php" title="View the teams participating in the league">>Teams</a></li>
+</ul></li>
+<li><a href="#">Players &#9662;</a>
+<ul class="pdropdown">
+<li><a href="players.php" title="View players in alphabetical order">Sorted by name</a></li>
+<li><a href="players.php?by=club" title="VIew players sorted by club">Sorted by club</a></li>
+<li><a href="players.php?by=rank" title="View players sorted by rank">Sorted by rank</a></li>
+<li><a href="players.php?by=clubrank" title="View players sorted by club then by rank">Sorted by club/rank</a></li>
+<li><a href="pbt.php" title="View players by team">Sorted by team</a></li>
+</ul></li>
+<li><a href="#">Matches &#9662;</a>
+<ul class="pdropdown">
+<li><a href="matches.php" title="View league matches for current season">Match statu</a></li>
+<li><a href="results.php" title="View results table for league matches">Results</a></li>
+</ul></li>
+<li><a href="#">League Table &#9662;</a>
+<ul class="pdropdown">
+<li><a href="league.php" title="Display current team league table">Current League table</a></li>
+<li><a href="leaguehist.php" title="Display historical league tables including individual league">Historical</a></li>
+<!-- <li><a href="ileague.php" class="il" title="View league table for Individual League">Individual League</a></li> -->
+</ul></li>
 <?php
 if ($logged_in) {
 //	print <<<EOT
-//<tr><td><a href="addilresult.php" class="il" title="For player use - add result in Individual League">Add IL Result</a></td></tr>
+//<li><a href="addilresult.php" class="il" title="For player use - add result in Individual League">Add IL Result</a></li>
 //
 //EOT;
 	if ($admin)  {
 		print <<<EOT
-<tr><td><a href="admin.php" class="memb" title="Administer the leagues">Admin menu</a></td></tr>
+<li><a href="admin.php" class="memb" title="Administer the leagues">Admin menu</a></li>
 
 EOT;
 	}
+	print <<<EOT
+<li><a href="#">User choices &#9662;</a>
+<ul class="pdropdown">
+
+EOT;
 	$qu = htmlspecialchars($username);
 	$nummsgs = num_unread_msgs();
 	if ($nummsgs == 0)  {
 		print <<<EOT
-<tr><td><a href="messages.php" title="Send and receive messages to/from opponents">Messages</a></td></tr>
+<li><a href="messages.php" title="Send and receive messages to/from opponents">Messages</a></li>
 
 EOT;
 	}
 	elseif($nummsgs == 1) {
 		print <<<EOT
-<tr><td><a href="messages.php" class="message" title="Send and receive messages to/from opponents">1 Message</a></td></tr>
+<li<a href="messages.php" class="message" title="Send and receive messages to/from opponents">1 Message</a></li>
 
 EOT;
 	}
 	else  {
 		print <<<EOT
-<tr><td><a href="messages.php" class="message" title="Send and receive messages to/from opponents">$nummsgs Messages</a></td></tr>
+<li><a href="messages.php" class="message" title="Send and receive messages to/from opponents">$nummsgs Messages</a></li>
 
 EOT;
 	}
 	print <<<EOT
-<tr><td><a href="osmatches.php" title="Display your outstanding games in team league">Outstanding</a></td></tr>
-<tr><td><a href="ownupd.php" title="Update your own account - rank etc">Update account</a></td></tr>
-<tr><td><a href="https://league.britgo.org/payments.php" title="Pay league subscriptions via PayPal">Pay subscriptions</a></td></tr>
-<tr><td><a href="logout.php" title="Log yourself out">Logout<br>$qu</a></td></tr>
+<li><a href="osmatches.php" title="Display your outstanding games in the league">Outstanding</a></li>
+<li><a href="ownupd.php" title="Update your own account - rank etc">Update account</a></li>
+<li><a href="https://league.britgo.org/payments.php" title="Pay league subscriptions via PayPal">Pay subscriptions</a></li>
+<li><a href="logout.php" title="Log yourself out">Logout<br>$qu</a></li>
+</ul></li>
 
 EOT;
 }
 ?>
-</table>
+</ul>
 <?php
 if (!$logged_in)  {
 	if (isset($_COOKIE['user_id']))
