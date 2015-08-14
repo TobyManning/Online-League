d0709fde812a25f935cf152e12fe9f8551c9d22c
John M Collins <jmc@toad.me.uk>
2015-08-14 19:40:45 +0100

    Try again at popups.

--- php/nav.php
+++ php/nav.php
@@ -26,69 +26,131 @@ print <<<EOT
 
 EOT;
 ?>
+<script type="text/javascript">
+var timeout	= 500;
+var closetimer	= 0;
+var ddmenuitem	= 0;
+
+// open hidden layer
+function mopen(id)
+{	
+	// cancel close timer
+	mcancelclosetime();
+
+	// close old layer
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+
+	// get new layer and show it
+	ddmenuitem = document.getElementById(id);
+	ddmenuitem.style.visibility = 'visible';
+
+}
+// close showed layer
+function mclose()
+{
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+}
+
+// go close timer
+function mclosetime()
+{
+	closetimer = window.setTimeout(mclose, timeout);
+}
+
+// cancel close timer
+function mcancelclosetime()
+{
+	if(closetimer)
+	{
+		window.clearTimeout(closetimer);
+		closetimer = null;
+	}
+}
+
+// close layer when click-out
+document.onclick = mclose;
+</script>
+
 <div class="innertube">
 <a href="http://www.britgo.org" title="Go to BGA main site">
 <img src="images/gohead12.gif" width="133" height="47" alt="BGA Logo" border="0" hspace="0" vspace="0"></a>
-<table>
-<tr><td><a href="index.php" title="Go to league home page">League Home</a></td></tr>
-<tr><td><a href="info.php" title="General Information">Information</a></td></tr>
-<tr><td><a href="playing.php" title="How to play in the leagues">Rules</a></td></tr>
-<tr><td><a href="news.php" title="Read about recent results and updates">News</a></td></tr>
-<tr><td><a href="clubs.php" title="View the list of clubs with members in the leagues">Clubs</a></td></tr>
-<tr><td><a href="teams.php" title="View the teams participating in the league">Teams</a></td></tr>
-<tr><td><a href="players.php" title="View players in the league">Players</a></td></tr>
-<tr><td class="subind"><a href="players.php?by=club" title="VIew players sorted by club">By club</a></td></tr>
-<tr><td class="subind"><a href="players.php?by=rank" title="View players sorted by rank">By rank</a></td></tr>
-<tr><td class="subind"><a href="players.php?by=clubrank" title="View players sorted by club then by rank">By club/rank</a></td></tr>
-<tr><td class="subind"><a href="pbt.php" title="View players by team">By team</a></td></tr>
-<tr><td><a href="matches.php" title="View team league matches for current season">Team Matches</a></td></tr>
-<tr><td><a href="results.php" title="View results table for team league matches">Team Results</a></td></tr>
-<tr><th colspan="2">League Table</th></tr>
-<tr><td class="subind"><a href="league.php" title="Display current team league table">Current</a></td></tr>
-<tr><td class="subind"><a href="leaguehist.php" title="Display historical league tables including individual league">Historical</a></td></tr>
-<!-- <tr><td class="subind"><a href="ileague.php" class="il" title="View league table for Individual League">Individual League</a></td></tr> -->
+<ul id="sddm">
+<li><a href="index.php" title="Go to league home page">League Home</a></li><br clear="ALL"/>
+<li><a href="#" onmouseover="mopen('m1')" onmouseout="mclosetime()">Information</a>
+<div id="m1" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+<a href="info.php" title="General Information">General Info</a>
+<a href="playing.php" title="How to play in the leagues">Rules</a>
+<a href="news.php" title="Read about recent results and updates">News</a>
+<a href="clubs.php" title="View the list of clubs with members in the leagues">Clubs</a>
+<a href="teams.php" title="View the teams participating in the league">Teams</a>
+</div></li><br clear="ALL"/>
+<li><a href="#" onmouseover="mopen('m2')" onmouseout="mclosetime()">Players</a>
+<div id="m2" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+<a href="players.php" title="View players in the league by name">By name</a>
+<a href="players.php?by=club" title="View players sorted by club">By club</a>
+<a href="players.php?by=rank" title="View players sorted by rank">By rank</a>
+<a href="players.php?by=clubrank" title="View players sorted by club then by rank">By club,rank</a>
+<a href="pbt.php" title="View players by team">By team</a>
+</div></li><br clear="ALL"/>
+<li><a href="#" onmouseover="mopen('m3')" onmouseout="mclosetime()">Matches</a>
+<div id="m3" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+<a href="matches.php" title="View league matches for current season">Matches</a><
+<a href="results.php" title="View results table for team league matches">Results</a>
+</div></li><br clear="ALL"/>
+<li><a href="#" onmouseover="mopen('m4')" onmouseout="mclosetime()">League tables</a>
+<div id="m4" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+<a href="league.php" title="Display current team league table">Current</a>
+<a href="leaguehist.php" title="Display historical league tables including individual league">Historical</a>
+<!-- <a href="ileague.php" class="il" title="View league table for Individual League">Individual League</a> -->
+</div></li><br clear="ALL"/>
 <?php
 if ($logged_in) {
 //	print <<<EOT
-//<tr><td><a href="addilresult.php" class="il" title="For player use - add result in Individual League">Add IL Result</a></td></tr>
+//<li><a href="addilresult.php" class="il" title="For player use - add result in Individual League">Add IL Result</a></li><br clear="ALL"/>
 //
 //EOT;
 	if ($admin)  {
 		print <<<EOT
-<tr><td><a href="admin.php" class="memb" title="Administer the leagues">Admin menu</a></td></tr>
+<li><a href="admin.php" class="memb" title="Administer the leagues">Admin menu</a></li><br clear="ALL"/>
 
 EOT;
 	}
 	$qu = htmlspecialchars($username);
 	$nummsgs = num_unread_msgs();
+	print <<<EOT
+<li><a href="#" onmouseover="mopen('m5')" onmouseout="mclosetime()">Account</a>
+<div id="m5" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+
+EOT;
 	if ($nummsgs == 0)  {
 		print <<<EOT
-<tr><td><a href="messages.php" title="Send and receive messages to/from opponents">Messages</a></td></tr>
+<a href="messages.php" title="Send and receive messages to/from opponents">Messages</a></li>
 
 EOT;
 	}
 	elseif($nummsgs == 1) {
 		print <<<EOT
-<tr><td><a href="messages.php" class="message" title="Send and receive messages to/from opponents">1 Message</a></td></tr>
+<a href="messages.php" class="message" title="Send and receive messages to/from opponents">1 Message</a>
 
 EOT;
 	}
 	else  {
 		print <<<EOT
-<tr><td><a href="messages.php" class="message" title="Send and receive messages to/from opponents">$nummsgs Messages</a></td></tr>
+<a href="messages.php" class="message" title="Send and receive messages to/from opponents">$nummsgs Messages</a>
 
 EOT;
 	}
 	print <<<EOT
-<tr><td><a href="osmatches.php" title="Display your outstanding games in team league">Outstanding</a></td></tr>
-<tr><td><a href="ownupd.php" title="Update your own account - rank etc">Update account</a></td></tr>
-<tr><td><a href="https://league.britgo.org/payments.php" title="Pay league subscriptions via PayPal">Pay subscriptions</a></td></tr>
-<tr><td><a href="logout.php" title="Log yourself out">Logout<br>$qu</a></td></tr>
+<a href="osmatches.php" title="Display your outstanding games in team league">Outstanding</a>
+<a href="ownupd.php" title="Update your own account - rank etc">Update account</a>
+<a href="https://league.britgo.org/payments.php" title="Pay league subscriptions via PayPal">Pay subscriptions</a>
+<a href="logout.php" title="Log yourself out">Logout<br>$qu</a>
+</div></li><br clear="ALL"/>
 
 EOT;
 }
 ?>
-</table>
+</ul>
 <?php
 if (!$logged_in)  {
 	if (isset($_COOKIE['user_id']))
