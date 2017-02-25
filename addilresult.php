00c133547cb1607bc38c6059ea6329ddc3eebc8e
TobyManning <ptm@tobymanning.co.uk>
2017-02-25 17:27:13 +0000

    Test commit to show Toby how to use Githib
    Please ignore this

--- addilresult.php
+++ addilresult.php
@@ -1,5 +1,6 @@
 <?php
 //   Copyright 2011 John Collins
+// Toby Manning was here!
 
 //   This program is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
